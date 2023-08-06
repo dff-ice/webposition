08e366f46d88e557541d25960e1b200a2f8663c7
CuSO4 <106749434+dff-ice@users.noreply.github.com>
2023-08-06 15:39:49 +0800

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,9 +3,9 @@ const pwd = document.querySelector('.input-wrapper:nth-child(3)')
 const num = document.querySelector('.input-wrapper:nth-child(5)')
 const phone = document.querySelector('.input-wrapper:nth-child(7)')
 // console.log(phone);
-uname.firstElementChild.addEventListener('blur',() => {
+uname.firstElementChild.addEventListener('blur', () => {
     console.log(/^[a-zA-Z0-9,.!?'"()\- ]*$/.test(uname.firstElementChild.value));
-    if(!/^[a-zA-Z0-9,.!?'"()\- ]*$/.test(uname.firstElementChild.value)){
+    if (!/^[a-zA-Z0-9,.!?'"()\- ]*$/.test(uname.firstElementChild.value)) {
         uname.nextElementSibling.innerHTML = '含有非法字符'
         uname.nextElementSibling.className = 'error'
         return
@@ -14,8 +14,8 @@ uname.firstElementChild.addEventListener('blur',() => {
     uname.nextElementSibling.innerHTML = ''
 
 })
-pwd.firstElementChild.addEventListener('blur',() => {
-    if(!/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9 _]{8,15}$/.test(pwd.firstElementChild.value)){
+pwd.firstElementChild.addEventListener('blur', () => {
+    if (!/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9 _]{8,15}$/.test(pwd.firstElementChild.value)) {
         // console.log(pwd.nextElementSibling);
         pwd.nextElementSibling.innerHTML = '至少包含一位大写字母、小写字母、数字,且长度在8-15之间'
         pwd.nextElementSibling.className = 'error'
@@ -24,8 +24,8 @@ pwd.firstElementChild.addEventListener('blur',() => {
     pwd.nextElementSibling.className = ''
     pwd.nextElementSibling.innerHTML = ''
 })
-phone.firstElementChild.addEventListener('blur',() => {
-    if(!/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(phone.firstElementChild.value)&&!/^[1-9]\d{4,11}$/.test(phone.firstElementChild.value)&&!/^[a-zA-Z0-9._+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(phone.firstElementChild.value)){
+phone.firstElementChild.addEventListener('blur', () => {
+    if (!/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(phone.firstElementChild.value) && !/^[1-9]\d{4,11}$/.test(phone.firstElementChild.value) && !/^[a-zA-Z0-9._+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(phone.firstElementChild.value)) {
         // console.log(phone.nextElementSibling);
         phone.nextElementSibling.innerHTML = '请输入正确的联系方式'
         phone.nextElementSibling.className = 'error'
@@ -34,8 +34,8 @@ phone.firstElementChild.addEventListener('blur',() => {
     phone.nextElementSibling.className = ''
     phone.nextElementSibling.innerHTML = ''
 })
-num.firstElementChild.addEventListener('blur',() => {
-    if(!("12061802" === num.firstElementChild.value)){
+num.firstElementChild.addEventListener('blur', () => {
+    if (!("12061802" === num.firstElementChild.value)) {
         // console.log(num.nextElementSibling);
         num.nextElementSibling.innerHTML = '请输入正确的号码'
         num.nextElementSibling.className = 'error'
@@ -48,19 +48,26 @@ num.firstElementChild.addEventListener('blur',() => {
 // -------------------------------------------------------------
 
 const cannot = document.querySelector('.x')
-cannot.addEventListener('click',() => {
+cannot.addEventListener('click', () => {
     cannot.parentElement.style.display = 'none'
 })
 
 //确认
 const verify = document.querySelector('input[type=button]')
 // console.log(verify);
-verify.addEventListener('click',() =>{
-    localStorage.setItem('data',JSON.stringify({
-        uname: `${uname.firstElementChild.value}`,
-        password: `${pwd.firstElementChild.value}`,
-        num: `${num.firstElementChild.value}`,
-        phone: `${phone.firstElementChild.value}`,
-        location: `经度：${longitude.toFixed(6)}, 纬度：${latitude.toFixed(6)}`
-    }))
+// console.log(localStorage.getItem('data'));
+verify.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (document.querySelector('.error') === null) {
+        localStorage.setItem('data', JSON.stringify({
+            uname: `${uname.firstElementChild.value}`,
+            password: `${pwd.firstElementChild.value}`,
+            num: `${num.firstElementChild.value}`,
+            phone: `${phone.firstElementChild.value}`,
+        }))
+        location.href = './succeed.html'
+    }
+    else{
+        alert('您填入的信息不完整或未进行定位操作')
+    }
 })
