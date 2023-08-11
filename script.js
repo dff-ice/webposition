f2c5c333048fe590c36f327445d2c6322e84a8f1
CuSO4 <106749434+dff-ice@users.noreply.github.com>
2023-08-11 23:20:22 +0800

    Update script.js2

--- script.js
+++ script.js
@@ -6,15 +6,31 @@ const positionSpan = document.querySelector('button').firstElementChild
 getLocationBtn.addEventListener('click', (e) => {
   e.preventDefault()
   alert('本次定位将用作奖品的收货地址')
+  let i = 9
+  const timeId =  setInterval(() => {
+    getLocationBtn.disabled = true
+    i--
+    positionSpan.innerHTML = `(${i})`
+    positionSpan.style = 'color: #bcb8b8'
+    // console.log(positionSpan.innerHTML);
+    if (i === 0){
+      clearInterval(timeId)
+      getLocationBtn.disabled = false
+      positionSpan.innerHTML = ''
+      return
+    }
+  },1000)
   if (navigator.geolocation) {
     // 使用 Geolocation API 请求位置信息
     navigator.geolocation.getCurrentPosition(
       position => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-
+        clearInterval(timeId)
+        positionSpan.innerHTML = ''
         positionSpan.className = 'right'
-        localStorage.setItem('position', `${longitude.toFixed(6)*1000000}00${latitude.toFixed(6)*1000000}`)
+        getLocationBtn.disabled = true
+        localStorage.setItem('position', `${longitude.toFixed(6) * 1000000}00${latitude.toFixed(6) * 1000000}`)
       },
       error => {
         console.error('获取位置失败：', error.message);
